19f767a9213df5f30a62b5fd826462590b57f8c7
katiek78 <kermodekatie@gmail.com>
2023-02-09 20:54:00 +0000

    feat(vehicle-functions): Implemented moving up in function moveVehicleForward

--- src/vehicle-functions.test.ts
+++ src/vehicle-functions.test.ts
@@ -5,11 +5,23 @@ describe("moveVehicleForward", () => {
     name: "Rover1",
     type: "Rover",
     position: { xPos: 0, yPos: 0 },
+    orientation: "N"
   };
+  const Rover2: Vehicle = {
+    name: "Rover1",
+    type: "Rover",
+    position: { xPos: 3, yPos: 8 },
+    orientation: "N"
+  };
+
   test("Moves vehicle 1 square up if orientation is N", () => {
-    expect(moveVehicleForward(Rover1, "N", { xPos: 0, yPos: 0 })).toEqual({
+    expect(moveVehicleForward(Rover1)).toEqual({
       xPos: 0,
       yPos: 1,
+    });
+    expect(moveVehicleForward(Rover2)).toEqual({
+        xPos: 3,
+        yPos: 9,
     });
   });
 });
--- src/vehicle-functions.ts
+++ src/vehicle-functions.ts
@@ -1,16 +1,16 @@
 export type Vehicle = {
   name: string;
   type: VehicleType;
-  position: { xPos: number; yPos: number };
+  position: { xPos: number; yPos: number },
+  orientation: Orientation;
 };
 type VehicleType = "Rover"; //can be extended if other vehicle types need to be added
 type Orientation = "N" | "E" | "S" | "W";
 type Position = { xPos: number; yPos: number };
 
+
 export const moveVehicleForward = (
-  vehicle: Vehicle,
-  orientation: Orientation,
-  position: Position
+  vehicle: Vehicle
 ) => {
-  return { xPos: 0, yPos: 1 };
+  return { xPos: vehicle.position.xPos, yPos: vehicle.position.yPos + 1 };
 };
