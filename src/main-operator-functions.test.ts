63e20853d87c0d85d563f6ff12546196902db393
katiek78 <kermodekatie@gmail.com>
2023-02-10 16:19:05 +0000

    feat(main-operator-functions): Added processTextInstructions function

---
+++ src/main-operator-functions.test.ts
@@ -0,0 +1,31 @@
+import {
+    processTextInstructions
+  } from "./main-operator-functions";
+
+  describe("processTextInstructions", () => {
+    const INSTRUCTIONS_NO_MOVEMENTS = 
+    `8 8
+1 2 N
+
+3 3 E`;
+
+    const INSTRUCTIONS_WITH_MOVEMENTS = 
+    `8 8
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM`;
+   
+    test("Returns original vehicle positions if movement list is empty", () => {
+      expect(processTextInstructions(INSTRUCTIONS_NO_MOVEMENTS)).toEqual(  
+        `1 2 N
+3 3 E`
+      );   
+    });
+    test("Returns new vehicle positions if movement instructions are given", () => {
+        expect(processTextInstructions(INSTRUCTIONS_WITH_MOVEMENTS)).toEqual(  
+          `1 3 N
+5 1 E`
+        );   
+      });
+});
---
+++ src/main-operator-functions.ts
@@ -0,0 +1,54 @@
+import { createGrid } from "./grid-functions";
+import { ORIENTATIONS, createVehicle, Vehicle, processMovementString } from "./vehicle-functions";
+
+export const processTextInstructions = (instructionList: string) => {
+    /*return instructionList.split('\n')
+    .filter(instructionLine => instructionLine !== '')
+    .map(instructionLine => instructionLine.trim())
+    .slice(1)
+    .join("\n");*/
+
+    //split into lines
+    //create a grid from first line
+    //create a vehicle from second line
+    //process movements from third line and add to output
+    //keep creating vehicles and processing movements
+
+    const instructionArray = instructionList.split('\n');
+    console.log(instructionArray);
+    let output = "";
+    
+    //create grid
+    const gridLine = instructionArray[0];
+    const maxX = parseInt(gridLine.split(" ")[0]);
+    const maxY = parseInt(gridLine.split(" ")[1]);
+    const grid = createGrid(maxX, maxY);
+        
+    for (let line=1; line < instructionArray.length; line+= 2) {
+     
+        //create vehicle
+        const vehicleDataLine = instructionArray[line];
+        const xPos = parseInt(vehicleDataLine.split(" ")[0]);
+        const yPos = parseInt(vehicleDataLine.split(" ")[1]);
+        const orientation = ORIENTATIONS[ORIENTATIONS.indexOf(vehicleDataLine.split(" ")[2])];        
+        if (ORIENTATIONS.indexOf(orientation) === -1) return null;
+        if (isNaN(xPos) || isNaN(yPos)) return null;        
+        const vehiclePosition = {xPos, yPos};
+        const vehicle = createVehicle("", "Rover", vehiclePosition, orientation, grid);
+        console.log(JSON.stringify(vehicle));
+
+        //process movements for above vehicle
+        if (line + 1 < instructionArray.length && instructionArray[line + 1] !== '') {
+            const newVehicleData = processMovementString(vehicle, grid, instructionArray[line+1]);
+            console.log(instructionArray[line+1]);
+            output += `${line == 1 ? '' : '\n'}${newVehicleData.position.xPos} ${newVehicleData.position.yPos} ${newVehicleData.orientation}`;            
+        } else {
+            output += `${line == 1 ? '' : '\n'}${vehicle.position.xPos} ${vehicle.position.yPos} ${vehicle.orientation}`;
+        }
+
+        
+    }
+
+    return output;
+};
+    
