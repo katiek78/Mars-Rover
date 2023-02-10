5e35b691b300fad20f63ffc2a12ca5fbbef038b6
katiek78 <kermodekatie@gmail.com>
2023-02-10 10:02:16 +0000

    feat(vehicle-functions): Implemented rotateVehicle

--- src/vehicle-functions.test.ts
+++ src/vehicle-functions.test.ts
@@ -1,37 +1,41 @@
-import { Vehicle, moveVehicleForward } from "./vehicle-functions";
+import {
+  Vehicle,
+  moveVehicleForward,
+  rotateVehicle,
+} from "./vehicle-functions";
+
+const Rover1: Vehicle = {
+  name: "Rover1",
+  type: "Rover",
+  position: { xPos: 0, yPos: 0 },
+  orientation: "N",
+};
+const Rover2: Vehicle = {
+  name: "Rover2",
+  type: "Rover",
+  position: { xPos: 3, yPos: 8 },
+  orientation: "N",
+};
+const Rover3: Vehicle = {
+  name: "Rover3",
+  type: "Rover",
+  position: { xPos: 3, yPos: 8 },
+  orientation: "W",
+};
+const Rover4: Vehicle = {
+  name: "Rover4",
+  type: "Rover",
+  position: { xPos: 3, yPos: 8 },
+  orientation: "S",
+};
+const Rover5: Vehicle = {
+  name: "Rover5",
+  type: "Rover",
+  position: { xPos: 3, yPos: 8 },
+  orientation: "E",
+};
 
 describe("moveVehicleForward", () => {
-  const Rover1: Vehicle = {
-    name: "Rover1",
-    type: "Rover",
-    position: { xPos: 0, yPos: 0 },
-    orientation: "N",
-  };
-  const Rover2: Vehicle = {
-    name: "Rover2",
-    type: "Rover",
-    position: { xPos: 3, yPos: 8 },
-    orientation: "N",
-  };
-  const Rover3: Vehicle = {
-    name: "Rover3",
-    type: "Rover",
-    position: { xPos: 3, yPos: 8 },
-    orientation: "W",
-  };
-  const Rover4: Vehicle = {
-    name: "Rover4",
-    type: "Rover",
-    position: { xPos: 3, yPos: 8 },
-    orientation: "S",
-  };
-  const Rover5: Vehicle = {
-    name: "Rover5",
-    type: "Rover",
-    position: { xPos: 3, yPos: 8 },
-    orientation: "E",
-  };
-
   test("Moves vehicle 1 square up if orientation is N", () => {
     expect(moveVehicleForward(Rover1)).toEqual({
       xPos: 0,
@@ -48,16 +52,31 @@ describe("moveVehicleForward", () => {
       yPos: 8,
     });
   });
-    test("Moves vehicle 1 square down if orientation is S", () => {
-      expect(moveVehicleForward(Rover4)).toEqual({
-        xPos: 3,
-        yPos: 7,
-      });
+  test("Moves vehicle 1 square down if orientation is S", () => {
+    expect(moveVehicleForward(Rover4)).toEqual({
+      xPos: 3,
+      yPos: 7,
+    });
   });
   test("Moves vehicle 1 square to the right if orientation is E", () => {
     expect(moveVehicleForward(Rover5)).toEqual({
       xPos: 4,
       yPos: 8,
     });
+  });
 });
+
+describe("rotateVehicle", () => {
+  test("Changes vehicle's orientation anticlockwise if direction is L", () => {
+    expect(rotateVehicle(Rover1, "L")).toEqual("W");
+    expect(rotateVehicle(Rover3, "L")).toEqual("S");
+    expect(rotateVehicle(Rover4, "L")).toEqual("E");
+    expect(rotateVehicle(Rover5, "L")).toEqual("N");
+  });
+  test("Changes vehicle's orientation clockwise if direction is R", () => {
+    expect(rotateVehicle(Rover1, "R")).toEqual("E");
+    expect(rotateVehicle(Rover3, "R")).toEqual("N");
+    expect(rotateVehicle(Rover4, "R")).toEqual("W");
+    expect(rotateVehicle(Rover5, "R")).toEqual("S");
+  });
 });
--- src/vehicle-functions.ts
+++ src/vehicle-functions.ts
@@ -1,26 +1,33 @@
 export type Vehicle = {
   name: string;
   type: VehicleType;
-  position: Position,
+  position: Position;
   orientation: Orientation;
 };
+
+const ORIENTATIONS = ["N", "E", "S", "W"] as const;
 type VehicleType = "Rover"; //can be extended if other vehicle types need to be added
-type Orientation = "N" | "E" | "S" | "W";
+type Orientation = typeof ORIENTATIONS[number];
 type Position = { xPos: number; yPos: number };
+type Direction = "L" | "R";
 
+export const moveVehicleForward = (vehicle: Vehicle) => {
+  switch (vehicle.orientation) {
+    case "N":
+      return { xPos: vehicle.position.xPos, yPos: vehicle.position.yPos + 1 };
+    case "E":
+      return { xPos: vehicle.position.xPos + 1, yPos: vehicle.position.yPos };
+    case "S":
+      return { xPos: vehicle.position.xPos, yPos: vehicle.position.yPos - 1 };
+    case "W":
+      return { xPos: vehicle.position.xPos - 1, yPos: vehicle.position.yPos };
+  }
+};
 
-export const moveVehicleForward = (
-  vehicle: Vehicle
-) => {
-    switch (vehicle.orientation) {
-        case ( "N"):
-            return { xPos: vehicle.position.xPos, yPos: vehicle.position.yPos + 1 };
-        case ("E"):
-            return { xPos: vehicle.position.xPos + 1, yPos: vehicle.position.yPos };
-        case ("S"):
-            return { xPos: vehicle.position.xPos, yPos: vehicle.position.yPos - 1 };
-        case ("W"):
-            return { xPos: vehicle.position.xPos - 1, yPos: vehicle.position.yPos };
-    }
-  
+export const rotateVehicle = (vehicle: Vehicle, direction: Direction) => {
+  if (vehicle.orientation === "N" && direction == "L") return "W";
+  if (vehicle.orientation === "W" && direction == "R") return "N";
+  return direction === "L"
+    ? ORIENTATIONS[ORIENTATIONS.indexOf(vehicle.orientation) - 1]
+    : ORIENTATIONS[ORIENTATIONS.indexOf(vehicle.orientation) + 1];
 };
