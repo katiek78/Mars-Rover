cd70b222ddf00421fc80d69b13aab122a609ad33
katiek78 <kermodekatie@gmail.com>
2023-02-11 11:52:37 +0000

    refactor(UI): Made code more DRY for in parseVehiclePositionOnRectangularGrid

--- src/ui/parse.ts
+++ src/ui/parse.ts
@@ -8,11 +8,8 @@ export const parseGridDimension = (input: string): number | undefined => {
 
 export const parseVehiclePositionOnRectangularGrid = (input: string, grid: Grid, dimension: Dimension): number | undefined => {
     if (isNaN(parseInt(input))) return undefined;
-    if (dimension === "X") {
-        if (parseInt(input) > grid.maxX || parseInt(input) < 0) return undefined;        
-    } else {
-        if (parseInt(input) > grid.maxY || parseInt(input) < 0) return undefined;        
-    }
+    const dimensionProperty = dimension === "X" ? "maxX" : "maxY";  
+    if (parseInt(input) > grid[dimensionProperty] || parseInt(input) < 0) return undefined;
     return parseInt(input);    
   };
   
