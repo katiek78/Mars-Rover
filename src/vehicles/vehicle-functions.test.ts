05985f02e6944421cad87385cf3b1d51e9767e80
katiek78 <kermodekatie@gmail.com>
2023-02-15 09:20:20 +0000

    test(vehicle-functions): Added test for different shape of plateau

--- src/vehicles/vehicle-functions.test.ts
+++ src/vehicles/vehicle-functions.test.ts
@@ -8,6 +8,7 @@ import {
 } from "./vehicle-functions";
 
 import { createGrid, RectangularGrid } from "../plateaus/grid-functions";
+import { Plateau } from "../plateaus/plateau-functions";
 
 const rover1: Rover = {
   position: { xPos: 0, yPos: 0 },
@@ -79,66 +80,76 @@ const rover8: Rover = {
   sampleCapacity: 3,
   samplesTaken: 3,
 };
-const GRID: RectangularGrid = createGrid(
+const grid: RectangularGrid = createGrid(
   8,
   8,
   [rover1],
   []
 );
+const PLATEAU: Plateau = {
+  vehicles: [],
+  samples: []
+};
 
 describe("moveVehicleForward", () => {  
   test("Moves vehicle 1 square up if orientation is N", () => {
-    expect(moveVehicleForward(rover1, GRID)).toEqual({
+    expect(moveVehicleForward(rover1, grid)).toEqual({
       xPos: 0,
       yPos: 1,
     });
   });
   test("Moves vehicle 1 square to the left if orientation is W", () => {
-    expect(moveVehicleForward(rover3, GRID)).toEqual({
+    expect(moveVehicleForward(rover3, grid)).toEqual({
       xPos: 2,
       yPos: 8,
     });
   });
   test("Moves vehicle 1 square down if orientation is S", () => {
-    expect(moveVehicleForward(rover4, GRID)).toEqual({
+    expect(moveVehicleForward(rover4, grid)).toEqual({
       xPos: 3,
       yPos: 7,
     });
   });
   test("Moves vehicle 1 square to the right if orientation is E", () => {
-    expect(moveVehicleForward(rover5, GRID)).toEqual({
+    expect(moveVehicleForward(rover5, grid)).toEqual({
       xPos: 4,
       yPos: 8,
     });
   });
   test("Moves vehicle 1 square to the right if orientation is E", () => {
-    expect(moveVehicleForward(rover5, GRID)).toEqual({
+    expect(moveVehicleForward(rover5, grid)).toEqual({
       xPos: 4,
       yPos: 8,
     });
   });
   test("Does not move vehicle N if already at top of grid", () => {
-    expect(moveVehicleForward(rover2, GRID)).toEqual({
+    expect(moveVehicleForward(rover2, grid)).toEqual({
       xPos: 3,
       yPos: 8,
     });
   });
   test("Does not move vehicle W if already at left of grid", () => {
-    expect(moveVehicleForward(rover6, GRID)).toEqual({
+    expect(moveVehicleForward(rover6, grid)).toEqual({
       xPos: 0,
       yPos: 5,
     });
   });
   test("Does not move vehicle S if already at bottom of grid", () => {
-    expect(moveVehicleForward(rover7, GRID)).toEqual({
+    expect(moveVehicleForward(rover7, grid)).toEqual({
       xPos: 2,
       yPos: 0,
     });
   });
   test("Does not move vehicle E if already at right of grid", () => {
-    expect(moveVehicleForward(rover8, GRID)).toEqual({
+    expect(moveVehicleForward(rover8, grid)).toEqual({
       xPos: 8,
       yPos: 6,
+    });
+  });
+  test("Does nothing if plateau is not a defined shape", () => {
+    expect(moveVehicleForward(rover1, PLATEAU)).toEqual({
+      xPos: 0,
+      yPos: 0,
     });
   });
 });
@@ -172,49 +183,49 @@ describe("createVehicle", () => {
 
 describe("processVehicleInstructions", () => {
   test("Returns original grid if instruction string is empty", () => {
-    expect(processVehicleInstructions(GRID, 0, "")).toEqual(GRID);
+    expect(processVehicleInstructions(grid, 0, "")).toEqual(grid);
   });
   test("Rotates vehicle clockwise if movement string is 'R'", () => {
-    expect(processVehicleInstructions(GRID, 0, "R")).toEqual({
-      ...GRID,
+    expect(processVehicleInstructions(grid, 0, "R")).toEqual({
+      ...grid,
       vehicles: [{ ...rover1, orientation: "E" }],
     });
   });
   test("Rotates vehicle anticlockwise if movement string is 'L'", () => {
-    expect(processVehicleInstructions(GRID, 0, "L")).toEqual({
-      ...GRID,
+    expect(processVehicleInstructions(grid, 0, "L")).toEqual({
+      ...grid,
       vehicles: [{ ...rover1, orientation: "W" }],
     });
   });
   test("Moves vehicle forward if movement string is 'M'", () => {
-    expect(processVehicleInstructions(GRID, 0, "M")).toEqual({
-      ...GRID,
+    expect(processVehicleInstructions(grid, 0, "M")).toEqual({
+      ...grid,
       vehicles: [{ ...rover1, position: { xPos: 0, yPos: 1 } }],
     });
   });
   test("Moves vehicle through multiple orientations", () => {
-    expect(processVehicleInstructions(GRID, 0, "LL")).toEqual({
-      ...GRID,
+    expect(processVehicleInstructions(grid, 0, "LL")).toEqual({
+      ...grid,
       vehicles: [{ ...rover1, orientation: "S" }],
     });
   });
   test("Moves vehicle through multiple forward movements", () => {
-    expect(processVehicleInstructions(GRID, 0, "MMMM")).toEqual({
-      ...GRID,
+    expect(processVehicleInstructions(grid, 0, "MMMM")).toEqual({
+      ...grid,
       vehicles: [{ ...rover1, position: { xPos: 0, yPos: 4 } }],
     });
   });
   test("Moves vehicle through multiple orientations and forward movements", () => {
-    expect(processVehicleInstructions(GRID, 0, "RMMMMLMML")).toEqual({
-      ...GRID,
+    expect(processVehicleInstructions(grid, 0, "RMMMMLMML")).toEqual({
+      ...grid,
       vehicles: [
         { ...rover1, orientation: "W", position: { xPos: 4, yPos: 2 } },
       ],
     });
   });
   test("Increases samplesTaken if an 'S' is found and adds the sample to the grid", () => {    
-    expect(processVehicleInstructions(GRID, 0, "S")).toEqual({
-      ...GRID,
+    expect(processVehicleInstructions(grid, 0, "S")).toEqual({
+      ...grid,
       vehicles: [{ ...rover1, samplesTaken: 1 }],
       samples: [{ xPos: 0, yPos: 0 }],
     });
